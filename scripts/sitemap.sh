7e77fc597fa3fdaf69f17f37557b832320c8985b
Black_Codec <f.dellorso@gmail.com>
2024-01-04 13:40:41 +0100

    aggiustato script for

--- scripts/sitemap.sh
+++ scripts/sitemap.sh
@@ -6,7 +6,7 @@ cd ${base_dir}
 [[ -f sitemap.xml ]] && mv ${base_dir}/sitemap.xml ${base_dir}/sitemap_old.xml
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > sitemap.xml
 echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" >> sitemap.xml
-for curr_file in $(find . -type f) {
+for curr_file in $(find . -type f); do
   if [[ "${curr_file}" == "feed-rss.xml" ]] || [[ "${curr_file}" == "sitemap.xml" ]] || [[ "${curr_file}" == "sitemap_old.xml" ]]; then
     continue
   fi
@@ -17,6 +17,6 @@ for curr_file in $(find . -type f) {
   echo "<loc>${name}</loc>" >> sitemap.xml
   echo "<lastmod>${mod}</lastmod>" >> sitemap.xml
   echo "</url>" >> sitemap.xml
-}
+done
 echo "</urlset>" >> sitemap.xml
 exit 0
