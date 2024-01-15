829a42f0dbbbbe46ebca2521a5978d052ab6b164
Black_Codec <f.dellorso@gmail.com>
2024-01-15 16:11:35 +0100

    sitemap script

--- scripts/sitemap.sh
+++ scripts/sitemap.sh
@@ -5,18 +5,22 @@ dest_file="${base_dir}/../sitemap.xml"
 bck_file="${base_dir}/../../backup/sitemap.xml"
 replacer="/./"
 [[ -f "${bck_file}" ]] && rm -f "${bck_file}"
-[[ -f "${dest_file}" ]] && mv "${dest_file}" "${bck_file}"
+[[ -f "${dest_file}" ]] && cp "${dest_file}" "${bck_file}"
 cd "${base_dir}"
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > "${dest_file}"
 echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" >> "${dest_file}"
+sed -i '$ d' "${dest_file}"
 for curr_file in $(find . -type f); do
   mod=$(date -r ${curr_file} +%Y-%m-%d)
   name=${curr_file%.*}
   name=$(echo "${base_url}/${name}")
-  echo "<url>" >> "${dest_file}"
-  echo "<loc>${name}</loc>" >> "${dest_file}"
-  echo "<lastmod>${mod}</lastmod>" >> "${dest_file}"
-  echo "</url>" >> "${dest_file}"
+  name=$(echo "${name/\/.\//}")
+  if [[ $(cat "${dest_file}" | grep "${name}" | wc -l) < 0 ]]; then
+    echo "<url>" >> "${dest_file}"
+    echo "<loc>${name}</loc>" >> "${dest_file}"
+    echo "<lastmod>${mod}</lastmod>" >> "${dest_file}"
+    echo "</url>" >> "${dest_file}"
+  fi
 done
 echo "</urlset>" >> "${dest_file}"
 sed -i "s|${replacer}|/|g" "${dest_file}"
